8ba6304a38da91497309313e47f9b9585edde6a4
GgeoffG <121970803+GgeoffG@users.noreply.github.com>
2023-04-21 11:53:43 -0700

    models filled in and model index relations created, beginning work on api routes

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -6,19 +6,34 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try{
+    const categoryData = await Category.findAll({
+      include:[{model: Product}]
+    });
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try{
+    const newCategory = await Category.create(req.body);
+    console.log('Category Created')
+    return res.json(newCategory)
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+  
 });
 
 router.delete('/:id', (req, res) => {
